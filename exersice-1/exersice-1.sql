fd24aa0a6e58f66d4ab8f4de081f24427afe19b7
erika flores <foglzerika@gmail.com>
2021-08-18 10:09:38 -0500

    feat| solution exersice-1

--- exersice-1/exersice-1.sql
+++ exersice-1/exersice-1.sql
@@ -1,1 +1,18 @@
 --Here goes your code
+CREATE DATABASE store;
+USE store;
+
+CREATE TABLE people (
+                        first_name varchar(20),
+                        last_name varchar(20) ,
+                        age INTEGER
+);
+
+INSERT INTO people(first_name, last_name, age) VALUES ("Ana" , "Olvera", 20);
+INSERT INTO people(first_name, last_name, age) VALUES ("Juan" , "Alcantara", 17);
+--Insert many
+INSERT INTO people(first_name, last_name, age) VALUES ("Laura" , "Casareal", 15),
+                                                      ("Jose" , "Torres", 16),
+                                                      ("Maria" , "Arias", 17);
+
+
