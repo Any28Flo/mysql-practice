d6c8828dd2f5f105de35f99203dc370a23754ad9
Erika Flores <foglzerika@gmail.com>
2021-11-22 10:29:07 -0600

    feat| add solution exersice-1

--- exersice-1/exersice-1.sql
+++ exersice-1/exersice-1.sql
@@ -15,4 +15,19 @@ INSERT INTO people(first_name, last_name, age) VALUES ("Laura" , "Casareal", 15)
                                                       ("Jose" , "Torres", 16),
                                                       ("Maria" , "Arias", 17);
 
+CREATE TABLE people_2 (
 
+                        first_name varchar(20) NOT NULL,
+                        last_name varchar(20) NOT NULL,
+                        age INTEGER NOT NULL
+);
+
+CREATE TABLE people_3 (
+
+                          first_name varchar(20) NOT NULL DEFAULT "s/n",
+                          last_name varchar(20) NOT NULL DEFAULT "S/",
+                          age INTEGER NOT NULL DEFAULT  0
+);
+
+INSERT INTO employees(id,last_name, fist_name, middle_name, age, current_status) VALUES(1,"flores","Erika" , "Glez", 28, "employed");
+INSERT INTO employees(last_name, fist_name ,age) VALUES("Olvera","Itzel", 26);
