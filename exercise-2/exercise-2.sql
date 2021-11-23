a839040230eabfbc7bdd861cb1034d132b13c3d0
Erik Daniel <erikrame69@gmail.com>
2021-11-22 21:48:17 -0600

    Execise 2

---
+++ exercise-2/exercise-2.sql
@@ -0,0 +1,17 @@
+--Code
+
+--Create the table pastries
+CREATE DATABASE pastries_db;
+
+--Use the table pastries
+USE pastries_db;
+
+--Create a parameters 
+CREATE TABLE pastries(name_pastries VARCHAR(50) NOT NULL, quantity INT NOT NULL);
+
+--Describe the pastries table
+DESC pastries;
+
+--DELETE the pastries table
+DROP TABLE pastries;
+
