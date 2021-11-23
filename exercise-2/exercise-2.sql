2ff06a5190a2a891c2d73a91d599287ae8a4d178
DannHdz <dann.hedz.98@gmail.com>
2021-11-23 03:07:55 -0600

    done

---
+++ exercise-2/exercise-2.sql
@@ -0,0 +1,16 @@
+
+CREATE DATABASE pastries_db;
+
+USE pastreis_db;
+
+CREATE TABLE pastries(
+    name VARCHAR(50),
+    quantity INT
+);
+
+SHOW TABLES;
+
+DESC pastries_db;
+
+DROP TABLE pastries_db;
+
