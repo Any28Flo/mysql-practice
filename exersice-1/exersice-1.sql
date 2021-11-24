86883dfe5fd2600f0dde3f1a434c8a8c9a1730be
iarielg <arielgonzalezdelacruz@gmail.com>
2021-11-23 21:40:24 -0600

    done

--- exersice-1/exersice-1.sql
+++ exersice-1/exersice-1.sql
@@ -1,1 +1,18 @@
 --Here goes your code
+create database store_db;
+use store_db;
+create table people (
+    id_person int not null auto_increment,
+    first_name char(20) limit not null,
+    last_name char(20) limit not null,
+    age int not null,
+);
+insert into people (first_name, last_name, age) values ('Ana', 'Olvera', 20);
+insert into people (first_name, last_name, age) values ('Juan', 'Alcantara', 17);
+insert into people (first_name, last_name, age) values ('Laura', 'Casareal', 15), ('Jose', 'Torres', 16), ('Maria', 'Arias', 17);
+
+select count(*) from people;
+
+drop table people;
+
+
