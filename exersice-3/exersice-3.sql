29350da7d70aa77a9f4465e81178db9f297fcc10
Carlos Trejo <78885685+CarlosTrej@users.noreply.github.com>
2021-11-23 20:09:26 -0600

    Update exersice-3.sql

--- exersice-3/exersice-3.sql
+++ exersice-3/exersice-3.sql
@@ -1,5 +1,51 @@
-SELECT * FROM pastries WHERE name="chocolate";
 
-SELECT * FROM pastries WHERE id=3;
 
+USE cats_store;
+
+SELECT cat_id FROM cats;
+/* +--------+
+| cat_id |
++--------+
+|      1 |
+|      2 |
+|      3 |
+|      4 |
+|      5 |
+|      6 |
+|      7 |
++--------+ */
+
+
+SELECT name,breed FROM cats;
+/* +----------------+------------+
+| name           | breed      |
++----------------+------------+
+| Ringo          | Tabby      |
+| Cindy          | Maine Coon |
+| Dumbledore     | Maine Coon |
+| Egg            | Persian    |
+| Misty          | Tabby      |
+| George Michael | Ragdoll    |
+| Jackson        | Sphynx     |
++----------------+------------+ */
+
+
+SELECT * FROM cats WHERE breed = "Tabby";
+/* +--------+-------+-------+------+
+| cat_id | name  | breed | age  |
++--------+-------+-------+------+
+|      1 | Ringo | Tabby |    4 |
+|      5 | Misty | Tabby |   13 |
++--------+-------+-------+------+ */
+
+
+SELECT cat_id, age FROM cats WHERE cat_id = age;
+
+/* +--------+------+
+| cat_id | age  |
++--------+------+
+|      4 |    4 |
+|      7 |    7 |
++--------+------+
+ */
 
