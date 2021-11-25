00d18169a56198245c407010cd181a5747aa2030
cristian-iram-monarres-mtz <cristian.monarres.mtz@gmail.com>
2021-11-25 00:44:26 -0600

    done

--- exersice-3/exersice-3.sql
+++ exersice-3/exersice-3.sql
@@ -1,5 +1,9 @@
-SELECT * FROM pastries WHERE name="chocolate";
+USE cats_store;
+dd
+SELECT cat_id FROM cats_store.cats; 
 
-SELECT * FROM pastries WHERE id=3;
+SELECT name, breed FROM cats_store.cats;
 
+SELECT *FROM cats_store.cats WHERE breed ="Tabby";
 
+SELECT cat_id, age FROM cats_store.cats WHERE cat_id=age;
